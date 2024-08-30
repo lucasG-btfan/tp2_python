08c336cc94f4a867cea4788943dbfb478591188c
Nicolas Navarrete <nicolassantiagonavarrete.nsn@gmail.com>
2024-08-30 13:02:59 -0300

    ej1 version 2

---
+++ python.tp2/ej1_2.py
@@ -0,0 +1,20 @@
+valorDecimal= input ("ingrese un numero decimal: ")
+numstr = str(valorDecimal)
+print(numstr)
+
+# SHORT (No existe el tipo de datos Short en python)
+
+# INT
+numEnt = int(valorDecimal) 
+print ("El numero en entero es:",numEnt)
+
+# LONG (No existe el tipo de datos Long en python)
+
+# FLOAT
+num_float= float(valorDecimal)
+print("El numero en decimal es:",num_float)
+
+
+
+
+print("La cantidad de digitos son:",len(valorDecimal))
