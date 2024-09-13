676f35cc7ba17a146b446e3c18a4c4aebe616475
JuaniVatri <j.vatri.aguilera@gmail.com>
2024-09-13 17:08:55 -0300

    Update ej3.py

--- python.tp2/ej3.py
+++ python.tp2/ej3.py
@@ -1,3 +1,8 @@
+"""3- Codifique un algoritmo que solicite el ingreso de un numero de 3 dígitos (100 - 999)
+y por medio del uso de las operaciones matemáticas módulo 10 y división por 10 efectué 
+la suma de los 3 dígitos del número. Ejemplo ingreso 563, salida del algoritmo 14. Plantee
+el algoritmo planteando métodos para su resolución."""
+
 num3dig = input("Ingresa un numero de 3 digitos (100 - 999) ")
 
 num = int(num3dig)
@@ -6,7 +11,7 @@ if num < 100 or num > 999:
     exit()
 
 # 563 // 100 = 5  +  56 % 10 = 6     + num % 10 = 3
-suma = num // 100 + (num // 10) % 10 + num % 10
+suma = (num // 100) + ((num // 10) % 10) + (num % 10)
 print(suma)
 
 #################################################################################################
