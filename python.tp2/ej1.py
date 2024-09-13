9cbea8afa7b11cc938385eb33252c32118a55c79
JuaniVatri <j.vatri.aguilera@gmail.com>
2024-09-13 16:53:27 -0300

    Update ej1.py
    (Vatri) cambios en el nombre de la variable y quité la resta de la longitud del numero

--- python.tp2/ej1.py
+++ python.tp2/ej1.py
@@ -1,7 +1,11 @@
+"""Codifique un programa que solicite el ingreso de un numero decimal y asigne el mismo a una variable valorDecimal, 
+aplique las operaciones de CASTING para convertir la variable a los siguientes tipos de datos, short, int, long, float 
+investigue las diferentes formas de lograr la conversión. Muestre por pantalla el resultado de las conversiones."""
+
 num_str= str (input ("ingrese un numero decimal: "))
 print(num_str)
-num_float= float(num_str)
-print("El numero en decimal es: ",num_float)
-numEnt = int(num_float) 
+valorDecimal= float(num_str)
+print("El numero en decimal es: ",valorDecimal)
+numEnt = int(valorDecimal) 
 print ("El numero en entero es: ",numEnt)
-print("La cantidad de digitos son: ",len(num_str)-1)
+print("La cantidad de digitos son: ",len(num_str))
