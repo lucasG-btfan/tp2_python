5f2976f061b0e88e43cf7460ec179ea5659aa081
JuaniVatri <j.vatri.aguilera@gmail.com>
2024-09-13 18:44:02 -0300

    Update ej5.py

--- python.tp2/ej5.py
+++ python.tp2/ej5.py
@@ -1,3 +1,5 @@
+#Solicite el ingreso de una cadena y elimine todos los espacios de la misma, muestre la cadena resultante.
+
 palabras=input("Diga una frase ")
 texto=(palabras.split())
 print("".join(texto))
