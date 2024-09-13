d7b3e3c810e2ab157070ba74ca5c33341550b3ac
JuaniVatri <j.vatri.aguilera@gmail.com>
2024-09-13 20:12:20 -0300

    Update ej13.py

--- python.tp2/ej13.py
+++ python.tp2/ej13.py
@@ -1,3 +1,5 @@
+#13- Pedir el ingreso de dos cadenas por por teclado, indicar si la segunda cadena se encuentra dentro de la primera.
+
 cadena1 = input("Ingresa una texto: ")
 cadena2 = input("Ingresa una palabra: ")
 result = cadena1.find(cadena2)
