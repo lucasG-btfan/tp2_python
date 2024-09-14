7ed8e45b5951dd77e4d8e5fdcac21a5a1bbf3830
JuaniVatri <j.vatri.aguilera@gmail.com>
2024-09-14 16:57:21 -0300

    Update ej21.py

--- python.tp2/ej21.py
+++ python.tp2/ej21.py
@@ -1,3 +1,8 @@
+"""21- Codifique un programa que solicite un número entero mayor a cero y que mediante
+recursión sume todos los números números naturales desde el número ingresado hasta 1.
+Ejemplo: Ingreso 10
+El programa debe sumar 10 + 9 + 8 +7 +6 + 5 + 4 + 3 + 2 + 1"""
+
 num = 0
 
 while num <= 0:
