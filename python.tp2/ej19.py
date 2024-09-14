f7e0fddfe9ab3816482f4f09d5d679949a310bc3
JuaniVatri <j.vatri.aguilera@gmail.com>
2024-09-14 13:01:13 -0300

    Update ej19.py

--- python.tp2/ej19.py
+++ python.tp2/ej19.py
@@ -1,3 +1,18 @@
+"""19- Cree una clase OperacionMatematica con dos atributos valor1 y valor2 y un atributo de nombre operación.
+Agregue a la clase los siguientes 5 métodos e implemente la lógica correspondiente:
+sumarNumeros()
+restarNumeros()
+multiplicarNumeros()
+dividirNumeros()
+El quinto método será el siguiente:
+aplicarOperacion(operacion){
+…………………..
+}
+Cree una clase Calculo que contenga un método main, donde cree una instancia de la clase OperacionMatematica, 
+asigne 2 valores para las variables de la instancia y ejecute la función aplicarOperacion, pasando como 
+parámetro primero “+”, después “-”, a continuación “*” y finalmente “/”. Muestre por pantalla el resultado de las operaciones."""
+
+
 class operacionMatematica:
     total = 0
     def __init__(self, valor1, valor2):
