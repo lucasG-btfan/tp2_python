d194fab0840f3af615ee98cec802a842731c794e
Nicolas Navarrete <nicolassantiagonavarrete.nsn@gmail.com>
2024-09-27 11:07:48 -0300

    ej4 terminado

--- python.tp2/ej4.py
+++ python.tp2/ej4.py
@@ -5,8 +5,8 @@ cantidad es 1390,55 se necesitan 6 billetes de 200, 1 billete de 100, 1 billete 
 de 20, 1 moneda de 0.50 y una moneda de 0.05 centavos. Plantee el algoritmo planteando métodos para su resolución."""
 
 
-"""billetes = [200, 100, 50, 20, 10, 5, 2, 1]
-monedas = [0.50, 0.25, 0.10, 0.05]
+billetes = [200, 100, 50, 20, 10, 5, 2, 1]
+monedas = [0.5, 0.25, 0.10]
 
 contador = 0
 
@@ -22,24 +22,13 @@ while num >= 1:
     i += 1
 
 i = 0
-while num > 0 and num < 1:
+while num >= 0.5 and num < 1:
     if num % monedas[i] != num:
         contador += num // monedas[i]
         num = num % monedas[i]
         print("Cantidad de monedas de ", monedas[i], str(int(contador)))
     contador = 0
-    i += 1"""
-
-
-import math
-num=int(input("Ingrese una cantidad de dinero: "))
-for billete in (200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05 ):
-     cocientereal=num/billete
-     cocienteaumentado= math.ceil(cocientereal) #lo redondeo hacia arriba porque se necesita
-                                                #pagar con el total de billetes divisores más
-                                                #  otro billete más que sería el que paga 
-                                                #la parte menor, el resto.
-     print(f"Debe pagar con {cocienteaumentado} billetes de ${billete}") 
+    i += 1
 
 
 
