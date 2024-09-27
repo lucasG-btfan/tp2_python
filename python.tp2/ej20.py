8d1a025af1757477b31eb58946eccdd8a65fddd4
Nicolas Navarrete <nicolassantiagonavarrete.nsn@gmail.com>
2024-09-27 12:16:30 -0300

    ej20

--- python.tp2/ej20.py
+++ python.tp2/ej20.py
@@ -1,89 +1,98 @@
+import math
 
-# Función para asegurarse de que el valor no sea 0
-def NumN(x):
-    if x != 0:
-        return x
-    else:
-        x = int(input("ERROR ingrese otro valor: "))
-        return NumN(x)
+class Fraccion():
 
-# Función para sumar fracciones
-def suma_d_fracc(a, b, c, d):
-    num1 = (a * d) + (c * b)
-    num2 = b * d
-    print(f"{num1}/{num2}")
+    def __init__(self, numerador, denominador):
+        self.numerador = numerador
+        self.denominador = denominador
 
-# Función para restar fracciones
-def restar_fracc(a, b, c, d):
-    num1 = (a * d) - (c * b)
-    num2 = b * d
-    print(f"{num1}/{num2}")
+    def __str__(self):
+        return f"{self.numerador}/{self.denominador}"
+    
 
-# Función para multiplicar fracciones
-def mult_fracc(a, b, c, d):
-    num1 = a * c
-    num2 = b * d
-    print(f"{num1}/{num2}")
+class OperacionesFraccion():
 
-# Función para dividir fracciones
-def div_fracc(a, b, c, d):
-    num1 = a * d
-    num2 = b * c
-    print(f"{num1}/{num2}")
+    @staticmethod
+    def sumarFracciones(fraccion1, fraccion2):
+        mcm = math.lcm(fraccion1.denominador , fraccion1.denominador)
+        numresult = (fraccion1.numerador * (mcm // fraccion1.denominador)) + (fraccion2.numerador * (mcm // fraccion2.denominador))
 
-class operacionesFraccion:
-    def __init__(self, numerador1, denominador1, numerador2, denominador2, fraccion1, fraccion2, OperacionesFraccion):
-        self.numerador1 = numerador1
-        self.denominador1 = denominador1
-        self.numerador2 = numerador2
-        self.denominador2 = denominador2
-        self.fraccion1 = fraccion1
-        self.fraccion2 = fraccion2
-        self.OperacionesFraccion = OperacionesFraccion
+        return Fraccion(numresult, mcm)
+    @staticmethod
+    def restarFracciones(fraccion1, fraccion2):
+        mcm = math.lcm( fraccion1.denominador , fraccion1.denominador)
+        numresult = (fraccion1.numerador * (mcm // fraccion1.denominador)) - (fraccion2.numerador * (mcm // fraccion2.denominador))
+        return Fraccion(numresult, mcm)
+    @staticmethod
+    def multiplicarFracciones(fraccion1, fraccion2):
+        denominador = fraccion1.denominador * fraccion2.denominador
+        numresult = fraccion1.numerador * fraccion2.numerador
 
-    # Realizamos un switch case para elegir la operación aritmética
-    def switch_case(self):
-        match self.OperacionesFraccion:
-            case 1:
-                print(f"{self.fraccion1} + {self.fraccion2} = ", end="")
-                suma_d_fracc(self.numerador1, self.denominador1, self.numerador2, self.denominador2)
-            case 2:
-                print(f"{self.fraccion1} - {self.fraccion2} = ", end="")
-                restar_fracc(self.numerador1, self.denominador1, self.numerador2, self.denominador2)
-            case 3:
-                print(f"{self.fraccion1} * {self.fraccion2} = ", end="")
-                mult_fracc(self.numerador1, self.denominador1, self.numerador2, self.denominador2)
-            case 4:
-                print(f"{self.fraccion1} / {self.fraccion2} = ", end="")
-                div_fracc(self.numerador1, self.denominador1, self.numerador2, self.denominador2)
-            case _:
-                print("Por favor, ingrese un número correcto, hubo un error")
+        return Fraccion(numresult, denominador)
+    @staticmethod
+    def dividirFracciones(fraccion1, fraccion2):
+        numresult = fraccion1.numerador * fraccion2.denominador
+        denominador = fraccion1.denominador * fraccion2.numerador
+
+        if denominador == 0: 
+            return print("La fraccion restultante tiene un denominador 0.")
+        else:
+            return Fraccion(numresult, denominador)
+        
+class Menu():
+    @staticmethod
+    def limpiar():
+        print("Ingrese el numerador")
+        num1 = int(input())
+        denominador  = 0 
+        while denominador == 0:
+            print("Ingrese el denominador ")
+            denominador = int(input())
+        return Fraccion(num1, denominador)
+    @staticmethod
+    def aplicarOperacion(fraccion1, fraccion2):
+        operacion = None
+        while operacion != 7:
+            print("---------------------------------")
+            print("Porfavor ingrese que operacion desea ejecutar ")
+            print(f"Los numeros actuales son {fraccion1} y {fraccion2} ")
+            print("1 - Suma")
+            print("2 - Resta")
+            print("3 - Multiplicacion")
+            print("4 - Division")
+            print("5 - Cambiar numeros Fraccion 1")
+            print("6 - Cambiar numeros Fraccion 2")
+            print("7 - Salir")
+            print("---------------------------------")
+            operacion = int(input())
+
+            match operacion:
+                case 1:
+                    fracSuma = OperacionesFraccion.sumarFracciones(fraccion1, fraccion2)
+                    print(fracSuma)
+                case 2:
+                    fracResta = OperacionesFraccion.restarFracciones(fraccion1, fraccion2)
+                    print(fracResta)
+                case 3:
+                    fracMult = OperacionesFraccion.multiplicarFracciones(fraccion1, fraccion2)
+                    print(fracMult)
+                case 4:
+                    fracDiv = OperacionesFraccion.dividirFracciones(fraccion1, fraccion2)
+                    print(fracDiv)
+                case 5:
+                    fraccion1.numerador = int(input("Porfavor ingrese el nuevo numerador de la fraccion 1 "))
+                    fraccion1.denominador = int(input("Porfavor ingrese el nuevo denominador de la fraccion 1 "))
+                case 6:
+                    fraccion2.numerador = int(input("Porfavor ingrese el nuevo numerador de la fraccion 2 "))
+                    fraccion2.denominador = int(input("Porfavor ingrese el nuevo denominador de la fraccion 2 "))
+                case 7:
+                    print("Saliendo del programa")
+
 
 def main():
-    # Ingresamos valores para el numerador y el denominador de la primera fracción
-    numerador1 = int(input("Ingrese el primer numerador: "))
-    numerador1 = NumN(numerador1)
-    denominador1 = int(input("Ingrese el primer denominador: "))
-    denominador1 = NumN(denominador1)
-    fraccion1 = f"{numerador1}/{denominador1}"
-    print("La primera fracción es:", fraccion1)
-
-    # Ingresamos valores para el numerador y el denominador de la segunda fracción
-    numerador2 = int(input("Ingrese el segundo numerador: "))
-    numerador2 = NumN(numerador2)
-    denominador2 = int(input("Ingrese el segundo denominador: "))
-    denominador2 = NumN(denominador2)
-    fraccion2 = f"{numerador2}/{denominador2}"
-    print("La segunda fracción es:", fraccion2)
-
-    # Solicitamos al usuario que ingrese la operación que desea realizar
-    OperacionesFraccion = int(input("Ingrese un número por la operación que desea realizar: +(1); -(2); *(3); /(4): "))
-
-    # Creamos una instancia de operacionesFraccion y llamamos a switch_case
-    operacion = operacionesFraccion(numerador1, denominador1, numerador2, denominador2, fraccion1, fraccion2, OperacionesFraccion)
-    operacion.switch_case()
-
-# Llamamos a main
-if __name__ == "__main__":
-    main()
+    acceso = Menu()
+    fraccion1 = acceso.limpiar()
+    fraccion2 = acceso.limpiar()
+    acceso.aplicarOperacion(fraccion1, fraccion2)
+main()
 
